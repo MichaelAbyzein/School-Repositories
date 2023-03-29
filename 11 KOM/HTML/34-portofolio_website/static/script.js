4b868d7408c067795c97fb12c36e27e211481751
Michael Abyzein <110386829+MichaelAbyzein@users.noreply.github.com>
2023-03-29 14:44:01 +0700

    Add files via upload

---
+++ 11 KOM/HTML/34-portofolio_website/static/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('.menu-toggle').addEventListener('click', function() {
+        document.querySelector('.menu-nav').classList.toggle('clicked');
+    })
+})
